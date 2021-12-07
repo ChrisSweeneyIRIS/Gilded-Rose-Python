23365c3b994762884da906e090e0f9472fa56fd0
Chris Sweeney <Christopher.Sweeney@IRIS.CO.UK>
2021-12-07 13:31:22 +0000

    Created variables for strings, improved logic and readability
    :)

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -5,32 +5,46 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        self.increase_quality(item, -1)
+            
+            sulfuras = "Sulfuras, Hand of Ragnaros"
+            backstage_passes = "Backstage passes to a TAFKAL80ETC concert"
+            brie = "Aged Brie"
+
+            if item.name == sulfuras:
+                continue
+            
+            if item.name != brie and item.name != backstage_passes:
+                self.increase_quality(item, -1)
+
             else:
-                max_quality = 50
-                if item.quality < max_quality:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.quality < max_quality:
-                        if item.sell_in < 6:
-                            self.increase_quality(item, 2)
-                        elif item.sell_in < 11:
-                            self.increase_quality(item, 1)
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                self.increase_quality(item, -1)
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
+                self.increase_quality(item, 1)
+
+                if item.name == backstage_passes:
+                    if item.sell_in < 6:
+                        self.increase_quality(item, 2)
+
+                    elif item.sell_in < 11:
                         self.increase_quality(item, 1)
 
+            item.sell_in -= 1
+            
+            if item.sell_in < 0:
+                
+                if item.name == brie:                    
+                    self.increase_quality(item, 1)                    
+                else:
+                    if item.name == backstage_passes:
+                        item.quality -= item.quality                        
+                    else:
+                        self.increase_quality(item, -1)
+
     def increase_quality(self, item, increment):
-        item.quality += increment
+        max_quality = 50
+        if item.quality < max_quality:
+            item.quality += increment
+        
+        if item.quality > max_quality:
+            item.quality = max_quality
+
+        if item.quality < 0:
+            item.quality = 0
